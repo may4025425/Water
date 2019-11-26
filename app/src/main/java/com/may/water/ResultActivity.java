de1df7c2176dc8dac624f17a12a422c17e42ff74
may4025425 <may4025425@gmail.com>
2019-11-26 15:18:02 +0800

    11/26 多國語言

--- app/src/main/java/com/may/water/ResultActivity.java
+++ app/src/main/java/com/may/water/ResultActivity.java
@@ -9,14 +9,16 @@ import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
-    private static final String TAG = ResultActivity.class.getSimpleName();//用public也可以
+    private static final String TAG = ResultActivity.class.getSimpleName();//用public也可以，private static final不打也可以，只是好辨認其(TAG)用途
+    private static final float DEFAULT_FEE = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         Intent intent = getIntent();
-        float price = intent.getFloatExtra(getString(R.string.extra_price),-1);
+        //float price = intent.getFloatExtra(getString(R.string.extra_price),-1);
+        float price = intent.getFloatExtra(getString(R.string.extra_price),DEFAULT_FEE);
         Log.d(TAG,price + "");
         //Log.d("ResultActivity",price + "");
         TextView edprice = findViewById(R.id.price);
